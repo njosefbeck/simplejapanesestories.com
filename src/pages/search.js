d8d0d0c944f418d77c6677eb849050302b51fc58
Nathan Beck <njosefbeck@gmail.com>
2020-03-20 09:52:22 -0500

    Add SEO component to search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../../components/seo"
 import PageHeader from "../components/page-header"
 import Search from "../components/search"
 
 export default () => (
   <Layout>
+    <SEO
+      title='Search | Simple Japanese Stories'
+      unbrandedTitle='Search'
+      description='Search through available stories for a particular Japanese or English phrase or word.'
+      slug='search'
+    />
     <PageHeader text="Search" />
     <Search />
   </Layout>
