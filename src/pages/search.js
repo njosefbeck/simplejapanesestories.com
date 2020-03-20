60ab5fb04283aa1689762ad91af8be60842fe41c
Nathan Beck <njosefbeck@gmail.com>
2020-03-20 15:30:46 -0500

    Fix import

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../../components/seo"
+import SEO from "../components/seo"
 import PageHeader from "../components/page-header"
 import Search from "../components/search"
 
